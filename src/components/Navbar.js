05547ef8facced4f1a25e65e63824037e743305c
alavry <alavry@users.noreply.github.com>
2018-11-04 13:46:40 -0500

    useless <figure>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,7 @@ const Navbar = () => (
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="BMCC Programming Club" style={{ width: '100%', 'max-height': '100%', 'padding-top': '7px' }} />
-          </figure>
+          <img src={logo} alt="BMCC Programming Club" style={{ width: '100%', 'max-height': '100%', 'padding-top': '7px' }} />
         </Link>
       </div>
       <div className="navbar-start">
